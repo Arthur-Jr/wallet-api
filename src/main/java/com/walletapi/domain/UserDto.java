1b85e363039c43c63d7cbba1dbad3a480fa64228
Arthur Junior <arthurjr1906@gmail.com>
2023-02-17 14:50:43 -0300

    feat: Add validations on dto

--- src/main/java/com/walletapi/domain/UserDto.java
+++ src/main/java/com/walletapi/domain/UserDto.java
@@ -1,10 +1,18 @@
 package com.walletapi.domain;
 
+import com.walletapi.exceptions.ExceptionsMessages;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 /**
  * user DTO.
  */
 public class UserDto {
+  @NotEmpty(message = ExceptionsMessages.EMPTY_USERNAME)
+  @Size(min = 3, message = ExceptionsMessages.USERNAME_SIZE)
   private String username;
+  
+  @NotEmpty(message = ExceptionsMessages.EMPTY_PASSWORD)
   private String password;
 
   public String getUsername() {
