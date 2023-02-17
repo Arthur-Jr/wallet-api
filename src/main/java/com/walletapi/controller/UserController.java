c9b9a949f3a76cd45fc1fc040b9794d91a4eba73
Arthur Junior <arthurjr1906@gmail.com>
2023-02-17 15:04:18 -0300

    fix: add PostMapping annotation

--- src/main/java/com/walletapi/controller/UserController.java
+++ src/main/java/com/walletapi/controller/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class UserController {
   @Autowired
   private UserService service;
 
+  @PostMapping
   public ResponseEntity<User> registerUser(@Valid @RequestBody UserDto userPayload) {
     User newUser = this.service.registerUser(userPayload);
     return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
