2962b67991b79ed50028a0d49471689889c1f5bb
Arthur Junior <arthurjr1906@gmail.com>
2023-02-17 14:50:16 -0300

    feat: Add Error messages

---
+++ src/main/java/com/walletapi/exceptions/ExceptionsMessages.java
@@ -0,0 +1,10 @@
+package com.walletapi.exceptions;
+
+/**
+ * Error messages list.
+ */
+public class ExceptionsMessages {
+  public static final String EMPTY_USERNAME = "Username is required!";
+  public static final String USERNAME_SIZE = "Username must have at least 3 characters!";
+  public static final String EMPTY_PASSWORD = "Password is required!";
+}
