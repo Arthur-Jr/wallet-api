8c0f12c17579d8f0f01bac87e2d28b0132029c13
Arthur Junior <arthurjr1906@gmail.com>
2023-02-25 11:40:24 -0300

    feat: Error messages update

--- src/main/java/com/walletapi/exceptions/ExceptionsMessages.java
+++ src/main/java/com/walletapi/exceptions/ExceptionsMessages.java
@@ -9,4 +9,6 @@ public class ExceptionsMessages {
   public static final String EMPTY_PASSWORD = "Password is required!";
   public static final String USERNAME_ALREADY_EXISTS = "Username already registered!";
   public static final String USER_NOT_FOUND = "User not found!";
+  public static final String INVALID_LOGIN = "Username or password is incorrect!";
+  public static final String INTERNAL = "Internal Error!";
 }
