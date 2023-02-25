840f0451f2ad975dce633b291573e26b4a24dedd
Arthur Junior <arthurjr1906@gmail.com>
2023-02-25 11:50:14 -0300

    feat: Main update

--- src/main/java/com/walletapi/WalletApiApplication.java
+++ src/main/java/com/walletapi/WalletApiApplication.java
@@ -2,23 +2,14 @@ package com.walletapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * main class.
  */
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class WalletApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WalletApiApplication.class, args);
-  }
-
-  @Bean
-  public PasswordEncoder getPassword() {
-    return new BCryptPasswordEncoder();
   }
 }
