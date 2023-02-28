e485e86912c2012c1a4f6b078c022bcc253f756d
Arthur Junior <arthurjr1906@gmail.com>
2023-02-27 21:20:45 -0300

    feat: Create remove expense method on controller and service

--- src/main/java/com/walletapi/controller/ExpenseController.java
+++ src/main/java/com/walletapi/controller/ExpenseController.java
@@ -4,9 +4,12 @@ import com.walletapi.domain.ExpenseDto;
 import com.walletapi.model.Expense;
 import com.walletapi.service.ExpenseService;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +32,11 @@ public class ExpenseController {
     return ResponseEntity.status(HttpStatus.CREATED).body(
         this.expenseService.addExpense(expensePayload)
     );
+  }
+
+  @DeleteMapping("/{expenseId}")
+  public ResponseEntity<String> removeExpense(@PathVariable UUID expenseId) {
+    this.expenseService.removeExpense(expenseId);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
   }
 }
--- src/main/java/com/walletapi/service/ExpenseService.java
+++ src/main/java/com/walletapi/service/ExpenseService.java
@@ -1,9 +1,11 @@
 package com.walletapi.service;
 
 import com.walletapi.domain.ExpenseDto;
+import com.walletapi.exceptions.ExpenseNotFoundException;
 import com.walletapi.model.Expense;
 import com.walletapi.model.User;
 import com.walletapi.repositories.UserRepository;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +26,7 @@ public class ExpenseService {
   public Expense addExpense(ExpenseDto expensePayload) {
     User user = this.userService.getUserByUsername();
     Expense newExpense = new Expense();
-    newExpense.setExpenseId(user.getExpensesList().size());
+    newExpense.setExpenseId(UUID.randomUUID());
     newExpense.setCurrency(expensePayload.getCurrency());
     newExpense.setValue(expensePayload.getValue());
     newExpense.setDescription(expensePayload.getDescription());
@@ -35,4 +37,24 @@ public class ExpenseService {
     this.userRepo.save(user);
     return newExpense;
   }
+
+  /**
+   * Expense remove method.
+   */
+  public void removeExpense(UUID expenseId) {
+    User user = this.userService.getUserByUsername();
+    Expense expenseToRemove = this.findExpenseById(user, expenseId);
+    user.getExpensesList().remove(expenseToRemove);
+    this.userRepo.save(user);
+  }
+
+  private Expense findExpenseById(User user, UUID expenseId) {
+    return user
+        .getExpensesList()
+        .stream()
+        .filter(x -> x.getExpenseId().equals(expenseId))
+        .findFirst()
+        .orElseThrow(ExpenseNotFoundException::new);
+  }
+
 }
