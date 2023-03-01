47490e37bceeeb62f0311ebc2c451d36677e1030
Arthur Junior <arthurjr1906@gmail.com>
2023-03-01 18:58:29 -0300

    feat: Add tests of find user by username on service

--- src/test/java/com/walletapi/unit/service/UserServiceTests.java
+++ src/test/java/com/walletapi/unit/service/UserServiceTests.java
@@ -3,6 +3,7 @@ package com.walletapi.unit.service;
 import com.mongodb.MongoException;
 import com.walletapi.domain.TokenResponse;
 import com.walletapi.domain.UserDto;
+import com.walletapi.exceptions.UserNotFoundException;
 import com.walletapi.jwt.JwtService;
 import com.walletapi.model.User;
 import com.walletapi.repositories.UserRepository;
@@ -18,12 +19,16 @@ import org.mockito.Mock;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.ArgumentMatchers.any;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
@@ -83,5 +88,46 @@ public class UserServiceTests {
     verify(this.encoder, times(1)).encode(this.userDto.getPassword());
     verify(this.userRepository, times(1)).save(any(User.class));
     verify(this.jwtService, times(0)).generateToken(this.user);
+  }
+
+  @Test
+  @DisplayName("Find user by username test: should find user by username on jwt")
+  void should_find_user_by_username() {
+    this.mockSecurityContextHolder();
+    when(this.userRepository.findByUsername(UserDataExample.USERNAME)).thenReturn(this.user);
+
+    User user = this.userService.getUserByUsername();
+    assertEquals(user.getUsername(), this.user.getUsername());
+    verify(this.userRepository, times(1))
+        .findByUsername(UserDataExample.USERNAME);
+    verify(SecurityContextHolder.getContext().getAuthentication(), times(1))
+        .getPrincipal();
+  }
+
+  @Test
+  @DisplayName("Find user by username test: should throw if user not found")
+  void should_throw_if_user_not_found() {
+    this.mockSecurityContextHolder();
+    when(this.userRepository.findByUsername(UserDataExample.USERNAME))
+        .thenThrow(UserNotFoundException.class);
+
+    Throwable exception = assertThrows(
+        UserNotFoundException.class, () -> this.userService.getUserByUsername()
+    );
+
+    assertNotNull(exception);
+    verify(SecurityContextHolder.getContext().getAuthentication(), times(1))
+        .getPrincipal();
+    verify(this.userRepository, times(1))
+        .findByUsername(UserDataExample.USERNAME);
+  }
+
+  private void mockSecurityContextHolder() {
+    Authentication auth = mock(Authentication.class);
+    SecurityContext securityContext = mock(SecurityContext.class);
+    when(securityContext.getAuthentication()).thenReturn(auth);
+    SecurityContextHolder.setContext(securityContext);
+    when(SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+        .thenReturn(UserDataExample.USERNAME);
   }
 }
