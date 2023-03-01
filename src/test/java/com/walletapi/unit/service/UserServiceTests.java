d37a7551edb63223cb60701e7a43a809fafc9e71
Arthur Junior <arthurjr1906@gmail.com>
2023-03-01 17:31:40 -0300

    feat: Create Register user tests

---
+++ src/test/java/com/walletapi/unit/service/UserServiceTests.java
@@ -0,0 +1,87 @@
+package com.walletapi.unit.service;
+
+import com.mongodb.MongoException;
+import com.walletapi.domain.TokenResponse;
+import com.walletapi.domain.UserDto;
+import com.walletapi.jwt.JwtService;
+import com.walletapi.model.User;
+import com.walletapi.repositories.UserRepository;
+import com.walletapi.service.UserService;
+import com.walletapi.util.UserDataExample;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("User Service tests:")
+public class UserServiceTests {
+  @InjectMocks
+  private UserService userService;
+
+  @Mock
+  private UserRepository userRepository;
+
+  @Mock
+  private JwtService jwtService;
+
+  @Mock
+  private PasswordEncoder encoder;
+
+  private final UserDto userDto = new UserDto();
+  private final User user = new User();
+  private final String JWT_TOKEN_EX = "token";
+
+  @BeforeEach
+  public void initEach() {
+    this.userDto.setUsername(UserDataExample.USERNAME);
+    this.userDto.setPassword(UserDataExample.PASSWORD);
+
+    this.user.setUsername(UserDataExample.USERNAME);
+    this.user.setPassword(UserDataExample.ENCODED_PASSWORD);
+  }
+
+  @Test
+  @DisplayName("Register user test: get userDto and return a Jwt token")
+  void should_register_new_user() {
+    when(this.encoder.encode(any(String.class))).thenReturn(UserDataExample.ENCODED_PASSWORD);
+    when(this.userRepository.save(any(User.class))).thenReturn(this.user);
+    when(this.jwtService.generateToken(this.user)).thenReturn(this.JWT_TOKEN_EX);
+    TokenResponse token = this.userService.registerUser(this.userDto);
+
+    assertNotNull(token);
+    assertEquals(token.getClass(), TokenResponse.class);
+    verify(this.userRepository, times(1)).save(any(User.class));
+    verify(this.jwtService, times(1)).generateToken(this.user);
+    verify(this.encoder, times(1)).encode(this.userDto.getPassword());
+  }
+
+  @Test
+  @DisplayName("Register user test: Throw a duplicity error if email already registered")
+  void should_throw_duplicity_error() {
+    when(this.encoder.encode(any(String.class))).thenReturn(UserDataExample.ENCODED_PASSWORD);
+    when(this.userRepository.save(any(User.class))).thenThrow(MongoException.class);
+
+    Throwable exception = assertThrows(
+        MongoException.class, () -> this.userService.registerUser(this.userDto)
+    );
+    assertNotNull(exception);
+    assertEquals(exception.getClass(), MongoException.class);
+    verify(this.encoder, times(1)).encode(this.userDto.getPassword());
+    verify(this.userRepository, times(1)).save(any(User.class));
+    verify(this.jwtService, times(0)).generateToken(this.user);
+  }
+}
---
+++ src/test/java/com/walletapi/util/UserDataExample.java
@@ -0,0 +1,7 @@
+package com.walletapi.util;
+
+public class UserDataExample {
+  public static final String USERNAME = "test@email.com";
+  public static final String PASSWORD = "testPass";
+  public static final String ENCODED_PASSWORD = "encodedPass";
+}
