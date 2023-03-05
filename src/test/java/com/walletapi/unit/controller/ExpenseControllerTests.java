480fe6c45a5f5d6d7fba74dcc361f93465dffc5d
Arthur Junior <arthurjr1906@gmail.com>
2023-03-05 15:22:01 -0300

    feat: Expense controller update, add jwt test

--- src/test/java/com/walletapi/unit/controller/ExpenseControllerTests.java
+++ src/test/java/com/walletapi/unit/controller/ExpenseControllerTests.java
@@ -10,15 +10,16 @@ import com.walletapi.exceptions.ExpenseNotFoundException;
 import com.walletapi.exceptions.UserNotFoundException;
 import com.walletapi.jwt.JwtService;
 import com.walletapi.model.Expense;
+import com.walletapi.model.User;
 import com.walletapi.service.ExpenseService;
 import com.walletapi.service.UserDetailsServiceImp;
 import com.walletapi.util.ExpenseDataExample;
+import com.walletapi.util.UserDataExample;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -38,7 +39,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(ExpenseController.class)
 @Import({WebSecurityConfig.class, ApplicationConfig.class, JwtService.class})
-@AutoConfigureMockMvc(addFilters = false)
 @DisplayName("Expenses controller tests:")
 public class ExpenseControllerTests {
   @Autowired
@@ -50,8 +50,15 @@ public class ExpenseControllerTests {
   @MockBean
   private UserDetailsServiceImp userDetailsService;
 
+  private final JwtService jwtService = new JwtService(
+      "25432A462D4A614E645267556B58703273357638782F413F4428472B4B625065"
+  );
+
+  private String token;
+
   private final ExpenseDto expenseDto = new ExpenseDto();
   private final Expense expense = new Expense();
+  private final User user = new User();
 
   @BeforeEach
   public void initEach() {
@@ -67,29 +74,53 @@ public class ExpenseControllerTests {
     this.expense.setDescription(ExpenseDataExample.description);
     this.expense.setCurrency(ExpenseDataExample.currency);
     this.expense.setExpenseId(UUID.randomUUID());
+
+    this.user.setUsername(UserDataExample.USERNAME);
+    this.user.setPassword(UserDataExample.ENCODED_PASSWORD);
+    this.user.addExpense(this.expense);
+
+    token = this.jwtService.generateToken(this.user);
   }
 
   @Test
   @DisplayName("Add new expense tests: should have status 201 and return a new expense")
   void add_new_expense_success_case() throws Exception {
     when(this.expenseService.addExpense(this.expenseDto)).thenReturn(this.expense);
-    ResultActions response = this.addNewExpense(this.expenseDto);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.addNewExpense(this.expenseDto, this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isCreated())
         .andExpect(jsonPath("$.expenseId").exists());
     verify(this.expenseService, times(1)).addExpense(this.expenseDto);
+    verify(this.userDetailsService, times(1))
+        .loadUserByUsername(this.user.getUsername());
+  }
+
+  @Test
+  @DisplayName("Add new expense tests: should have status 403 if there is no token")
+  void add_new_expense_forbidden_error_case() throws Exception {
+    when(this.expenseService.addExpense(this.expenseDto)).thenReturn(this.expense);
+    ResultActions response = this.addNewExpense(this.expenseDto, null);
+
+    response.andExpect(status().isForbidden());
+    verify(this.expenseService, times(0)).addExpense(this.expenseDto);
+    verify(this.userDetailsService, times(0))
+        .loadUserByUsername(this.user.getUsername());
   }
 
   @Test
   @DisplayName("Remove expense tests: should have status 204 and return nothing")
   void remove_expense_success_case() throws Exception {
     doNothing().when(this.expenseService).removeExpense(this.expense.getExpenseId());
-    ResultActions response = this.removeExpense(this.expense.getExpenseId());
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.removeExpense(this.expense.getExpenseId(), this.token);
 
     response.andExpect(status().isNoContent());
     verify(this.expenseService, times(1))
         .removeExpense(this.expense.getExpenseId());
+    verify(this.userDetailsService, times(1))
+        .loadUserByUsername(this.user.getUsername());
   }
 
   @Test
@@ -98,13 +129,16 @@ public class ExpenseControllerTests {
     doThrow(new UserNotFoundException())
         .when(this.expenseService)
         .removeExpense(this.expense.getExpenseId());
-    ResultActions response = this.removeExpense(this.expense.getExpenseId());
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.removeExpense(this.expense.getExpenseId(), this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.message").value(ExceptionsMessages.USER_NOT_FOUND));
     verify(this.expenseService, times(1))
         .removeExpense(this.expense.getExpenseId());
+    verify(this.userDetailsService, times(1))
+        .loadUserByUsername(this.user.getUsername());
   }
 
   @Test
@@ -113,11 +147,29 @@ public class ExpenseControllerTests {
     doThrow(new ExpenseNotFoundException())
         .when(this.expenseService)
         .removeExpense(this.expense.getExpenseId());
-    ResultActions response = this.removeExpense(this.expense.getExpenseId());
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.removeExpense(this.expense.getExpenseId(), this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.message").value(ExceptionsMessages.EXPENSE_NOT_FOUND));
+    verify(this.userDetailsService, times(1))
+        .loadUserByUsername(this.user.getUsername());
+  }
+
+  @Test
+  @DisplayName("Remove expense tests: should have status 403 if there is no token")
+  void remove_expense_forbidden_error_case() throws Exception {
+    doThrow(new ExpenseNotFoundException())
+        .when(this.expenseService)
+        .removeExpense(this.expense.getExpenseId());
+    ResultActions response = this.removeExpense(this.expense.getExpenseId(), null);
+
+    response.andExpect(status().isForbidden());
+    verify(this.userDetailsService, times(0))
+        .loadUserByUsername(this.user.getUsername());
+    verify(this.userDetailsService, times(0))
+        .loadUserByUsername(this.user.getUsername());
   }
 
   @Test
@@ -125,14 +177,18 @@ public class ExpenseControllerTests {
   void edit_expense_success_case() throws Exception {
     when(this.expenseService.editExpense(this.expense.getExpenseId(), this.expense))
         .thenReturn(this.expense);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
 
-    ResultActions response = this.editExpense(this.expense, this.expense.getExpenseId());
+    ResultActions response =
+        this.editExpense(this.expense, this.expense.getExpenseId(), this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.expenseId").exists());
     verify(this.expenseService, times(1))
         .editExpense(this.expense.getExpenseId(), this.expense);
+    verify(this.userDetailsService, times(1))
+        .loadUserByUsername(this.user.getUsername());
   }
 
   @Test
@@ -141,14 +197,18 @@ public class ExpenseControllerTests {
     doThrow(new UserNotFoundException())
         .when(this.expenseService)
         .editExpense(this.expense.getExpenseId(), this.expense);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
 
-    ResultActions response = this.editExpense(this.expense, this.expense.getExpenseId());
+    ResultActions response =
+        this.editExpense(this.expense, this.expense.getExpenseId(), this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.message").value(ExceptionsMessages.USER_NOT_FOUND));
     verify(this.expenseService, times(1))
         .editExpense(this.expense.getExpenseId(), this.expense);
+    verify(this.userDetailsService, times(1))
+        .loadUserByUsername(this.user.getUsername());
   }
 
   @Test
@@ -157,19 +217,43 @@ public class ExpenseControllerTests {
     doThrow(new ExpenseNotFoundException())
         .when(this.expenseService)
         .editExpense(this.expense.getExpenseId(), this.expense);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
 
-    ResultActions response = this.editExpense(this.expense, this.expense.getExpenseId());
+    ResultActions response =
+        this.editExpense(this.expense, this.expense.getExpenseId(), this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.message").value(ExceptionsMessages.EXPENSE_NOT_FOUND));
+    verify(this.expenseService, times(1))
+        .editExpense(this.expense.getExpenseId(), this.expense);
+    verify(this.userDetailsService, times(1))
+        .loadUserByUsername(this.user.getUsername());
+  }
+
+  @Test
+  @DisplayName("Edit expense tests: should have status 403 if there is no token")
+  void edit_expense_forbidden_error_case() throws Exception {
+    doThrow(new ExpenseNotFoundException())
+        .when(this.expenseService)
+        .editExpense(this.expense.getExpenseId(), this.expense);
+
+    ResultActions response =
+        this.editExpense(this.expense, this.expense.getExpenseId(), null);
+
+    response.andExpect(status().isForbidden());
+    verify(this.expenseService, times(0))
+        .editExpense(this.expense.getExpenseId(), this.expense);
+    verify(this.userDetailsService, times(0))
+        .loadUserByUsername(this.user.getUsername());
   }
 
   @Test
   @DisplayName("Expense DTO validation tests: empty tag test")
   void empty_tag_test() throws Exception {
     this.expenseDto.setTag(null);
-    ResultActions response = this.addNewExpense(this.expenseDto);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.addNewExpense(this.expenseDto, this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isBadRequest())
@@ -180,7 +264,8 @@ public class ExpenseControllerTests {
   @DisplayName("Expense DTO validation tests: empty method test")
   void empty_method_test() throws Exception {
     this.expenseDto.setMethod(null);
-    ResultActions response = this.addNewExpense(this.expenseDto);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.addNewExpense(this.expenseDto, this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isBadRequest())
@@ -191,7 +276,8 @@ public class ExpenseControllerTests {
   @DisplayName("Expense DTO validation tests: empty value test")
   void empty_value_test() throws Exception {
     this.expenseDto.setValue(null);
-    ResultActions response = this.addNewExpense(this.expenseDto);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.addNewExpense(this.expenseDto, this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isBadRequest())
@@ -202,7 +288,8 @@ public class ExpenseControllerTests {
   @DisplayName("Expense DTO validation tests: empty description test")
   void empty_description_test() throws Exception {
     this.expenseDto.setDescription(null);
-    ResultActions response = this.addNewExpense(this.expenseDto);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.addNewExpense(this.expenseDto, this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isBadRequest())
@@ -213,25 +300,47 @@ public class ExpenseControllerTests {
   @DisplayName("Expense DTO validation tests: empty currency test")
   void empty_currency_test() throws Exception {
     this.expenseDto.setCurrency(null);
-    ResultActions response = this.addNewExpense(this.expenseDto);
+    when(this.userDetailsService.loadUserByUsername(this.user.getUsername())).thenReturn(this.user);
+    ResultActions response = this.addNewExpense(this.expenseDto, this.token);
 
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.message").value(ExceptionsMessages.EMPTY_CURRENCY));
   }
 
-  private ResultActions editExpense(Expense payload, UUID expenseId) throws Exception {
+  private ResultActions editExpense(Expense payload, UUID expenseId, String tkn) throws Exception {
+    if (tkn != null) {
+      return this.mockMvc.perform(put("/expense/{expenseId}", expenseId)
+          .header("Authorization", "Bearer " + tkn)
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(new ObjectMapper().writeValueAsString(payload))
+      );
+    }
+
     return this.mockMvc.perform(put("/expense/{expenseId}", expenseId)
         .contentType(MediaType.APPLICATION_JSON)
         .content(new ObjectMapper().writeValueAsString(payload))
     );
   }
 
-  private ResultActions removeExpense(UUID expenseId) throws Exception {
+  private ResultActions removeExpense(UUID expenseId, String token) throws Exception {
+    if (token != null) {
+      return this.mockMvc.perform(delete("/expense/{expenseId}", expenseId)
+          .header("Authorization", "Bearer " + token));
+    }
+
     return this.mockMvc.perform(delete("/expense/{expenseId}", expenseId));
   }
 
-  private ResultActions addNewExpense(ExpenseDto payload) throws Exception {
+  private ResultActions addNewExpense(ExpenseDto payload, String token) throws Exception {
+    if (token != null) {
+      return this.mockMvc.perform(post("/expense")
+          .header("Authorization", "Bearer " + token)
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(new ObjectMapper().writeValueAsString(payload))
+      );
+    }
+
     return this.mockMvc.perform(post("/expense")
         .contentType(MediaType.APPLICATION_JSON)
         .content(new ObjectMapper().writeValueAsString(payload))
